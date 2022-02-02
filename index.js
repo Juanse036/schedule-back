31994bbdaed0f1af7a60d731025fd313a3c3e08a
Juanse036 <juansebas036@gmail.com>
2022-02-02 11:01:20 -0500

    Cambio opciones CORS

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ const app = express();
 conetcarDB();
 // habilitar Cors
 console.log ( 'FrontEnd : ', process.env.FRONTEND_URL )
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
+const opcionesCors = {
+  origin: process.env.FRONTEND_URL
 }
 //
 app.use( cors( opcionesCors ) );
